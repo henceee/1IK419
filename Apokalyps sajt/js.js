b410efe742662b45c95e77e150f05e4934763640
henceee <hg222dv@student.lnu.se>
2014-06-03 16:17:03 +0200

    .

--- Apokalyps sajt/js.js
+++ Apokalyps sajt/js.js
@@ -8,16 +8,6 @@ var media = {
 	 var medialink = medialinklist.getElementsByTagName("a")[0];
 	   
 	 console.log(medialink);
-	 
-	 var dates = document.getElementById("dates");
-	 console.log(dates);
-	//getElementsByTagName("a")[0];
-	
-	
-	var wrapper = document.getElementById("wrapper");
-
-	console.log(wrapper);	
-
 	
 
 	 medialink.onclick= function(){
